5613baf446819ae1e30bfbbafc45142e6458d122
oscar alfredo <oscar.alfredo@academy.local>
2026-03-24 11:01:39 -0300

    fix:se reparo la aleatoriedad para el mapa de juego

--- display.py
+++ display.py
@@ -2,12 +2,11 @@ import random
 filas=[]
 
 def matriz(dimensionx,dimensiony,obstaculo,filas):
-    for i in range(dimensionx):
+    for i in range(dimensionx): #
         columna=[]
-        if obstaculo==True:
-            aleatorio=random.randint(1,5)
+        if obstaculo==True:  
             for j in range(dimensiony):
-
+                aleatorio=random.randint(1,5)
                 if aleatorio ==3:
                     columna.append("|⚫|")#obstaculo
                 else:
@@ -15,10 +14,10 @@ def matriz(dimensionx,dimensiony,obstaculo,filas):
         else:
             for j in range(dimensiony):
                 columna.append("|  |")#celda vacia
-                filas.append(columna)
+        filas.append(columna)
     #print(columna,i)
     for x in range(dimensionx):
         print(filas[x])
 
 
-matriz(4,3,False,filas)
+matriz(4,4,True,filas)
