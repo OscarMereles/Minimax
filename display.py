59ec76caa2c5c2c7d34ad42e9165c8d4c8cca228
oscar alfredo <oscar.alfredo@academy.local>
2026-03-24 10:51:35 -0300

    fix:se reparo la matriz bae para el mapa de juego

--- display.py
+++ display.py
@@ -1,20 +1,24 @@
 import random
 filas=[]
 
-def jugar(dimensionx,dimensiony,obstaculo,filas):
-    for i in range(0,dimensionx,1):
+def matriz(dimensionx,dimensiony,obstaculo,filas):
+    for i in range(dimensionx):
+        columna=[]
         if obstaculo==True:
             aleatorio=random.randint(1,5)
-            columna=[]
-            if aleatorio ==3:
-                columna.append("|⚫|")
-            else:
-                columna.append("|  |")
+            for j in range(dimensiony):
+
+                if aleatorio ==3:
+                    columna.append("|⚫|")#obstaculo
+                else:
+                    columna.append("|  |")#celda vacia
         else:
-            columna.append("|  |")
-    filas.append(columna)
-    print(columna,i)
+            for j in range(dimensiony):
+                columna.append("|  |")#celda vacia
+                filas.append(columna)
+    #print(columna,i)
     for x in range(dimensionx):
         print(filas[x])
 
-jugar(4,3,True,filas)
+
+matriz(4,3,False,filas)
