66b15c19749f41147c912e9bb5689c0723e45d07
Oscar Mereles <oscar.mereles97@gmail.com>
2026-03-29 13:16:51 -0300

    feat: se añadieron funciones de validar movimientos, obtener movimientos validos, y distancia chebyshev

--- MinimaxReseteado.py
+++ MinimaxReseteado.py
@@ -1,11 +1,12 @@
-import random,os
-tablero=[]
-xtablero=10
-ytablero=10
+import random,os    #se importan librerias para random y para limpiar la consola
+tablero=[]          #se crea la lista quue va a guardar el tablero donde se desarrolla la partida
+xtablero=10       #se define la
+ytablero=random.randint(4,15)
 coord_raton=[0,0]
 coord_gato=[xtablero-1,ytablero-1]
-coord_queso=[xtablero//2,xtablero//2]
+coord_queso=[xtablero//2,ytablero//2]
 coord_salida=[0,ytablero-1]
+
 def generar_tablero(xtablero,ytablero,tablero,obstaculos,coord_raton,coord_gato,coord_queso,coord_salida):
     
     for i in range(xtablero):
@@ -13,23 +14,29 @@ def generar_tablero(xtablero,ytablero,tablero,obstaculos,coord_raton,coord_gato,
         for j in range(ytablero):
             if coord_raton!=[i,j] and coord_gato!=[i,j] and coord_queso!=[i,j] and coord_salida!=[i,j]:
                 if obstaculos==True:
-                    aleatorio =random.randint(1,4)
+                    aleatorio =random.randint(1,3)
                     if aleatorio==1:
-                        fila.append("|####|")
+                        #fila.append("|####|")
+                        fila.append("|⬛|")
                     else:
-                        fila.append("|____|")
+                        #fila.append("|____|")
+                        fila.append("|__|")
                 else:
-                    fila.append("|____|")
+                    #fila.append("|____|")
+                    fila.append("|__|")
             elif coord_raton==[i,j]:
-                fila.append("|ᘛ⁐̤ᕐᐷ|")
+                #fila.append("|ᘛ⁐̤ᕐᐷ|")
+                fila.append("|🐭|")
             elif coord_gato==[i,j]:
-                fila.append("|ᓚᘏᗢ |")
+                #fila.append("|ᓚᘏᗢ |")
+                fila.append("|🐱|")
             elif coord_queso==[i,j]:
-                fila.append("|⪩. .|")
+                #fila.append("|⪩. .|")
+                fila.append("|🧀|")
             elif coord_salida==[i,j]:
-                fila.append("|exit|")
+                #fila.append("|exit|")
+                fila.append("|🚪|")
         tablero.append(fila)
-
 
 def mostrar_tablero(xtablero,ytablero,tablero):
     os.system('cls')
@@ -40,9 +47,17 @@ def mostrar_tablero(xtablero,ytablero,tablero):
         print(cadena)
         cadena=""
         
-def validar_movimiento(x,y,tablero):
-    return tablero[x][y]!="|####|"
+def validar_movimiento(x, y, tablero):
+    #return 0 <= x < len(tablero) and 0 <= y < len(tablero[0]) and tablero[x][y] != "|####|"
+    return 0 <= x < len(tablero) and 0 <= y < len(tablero[0]) and tablero[x][y] != "|⬛|"
+
+def obtener_movimientos_validos(x, y, tablero):
+    movimientos=[]
+    direcciones=[(-1,-1),(-1,0),(-1,1),(0,-1),(0,1),(1,-1),(1,0),(1,1)]
+
+def distancia_chebyshev(punto_a,punto_b):
+    distancia=max(abs(punto_a[0]-punto_b[0]),abs(punto_a[1]-punto_b[1]))
+    return distancia
 
 generar_tablero(xtablero,ytablero,tablero,True,coord_raton,coord_gato,coord_queso,coord_salida)
 mostrar_tablero(xtablero,ytablero,tablero)
-print(validar_movimiento(1,0,tablero))
